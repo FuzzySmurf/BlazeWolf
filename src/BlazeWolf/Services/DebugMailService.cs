ad5bd4963aeb1f07c5ba5063096f36ff20b2d3a7
Alex Moreno <alexmoreno777@gmail.com>
2016-02-18 10:19:20 -0800

    BlazeWolf Ini

---
+++ src/BlazeWolf/Services/DebugMailService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazeWolf.Services
+{
+    public class DebugMailService : IMailService
+    {
+        public bool SendMail(string to, string @from, string subject, string body)
+        {
+            //using string interpolation c# 6 feature
+            Debug.WriteLine($"Sending mail: To: {to}, Subject: {subject}");
+            return true;
+        }
+    }
+}
---
+++ src/BlazeWolf/wwwroot/js/site.js
@@ -0,0 +1,29 @@
+﻿/* Site.js */
+(function () {
+
+    //var ele = document.getElementById("username");
+    var ele = $("#username");
+    ele.innerHTML = "test user";
+
+    var main = $("#main");
+    main.on("mouseenter", function() {
+        main.style["background-color"] = "#888";
+    });
+    main.on("mouseleave", function() {
+        main.style["background-color"] = "";
+    });
+
+    var $sidebarAndWrapper = $("#sidebar,#wrapper");
+    var $icon = $("#sidebarToggle i.fa");
+
+    $("#sidebarToggle").on("click", function() {
+        $sidebarAndWrapper.toggleClass("hide-sidebar");
+        if ($sidebarAndWrapper.hasClass("hide-sidebar")) {
+            $icon.removeclass("fa-angle-right");
+            $icon.addclass("fa-angle-left");
+        } else {
+            $icon.removeclass("fa-angle-left");
+            $icon.addclass("fa-angle-right");
+        }
+    });
+})();
